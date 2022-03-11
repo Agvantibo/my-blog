59eda612ed447e245a5052391835f808e7e5dcd5
Agvantibo <derez08@yandex.ru>
2022-03-11 16:56:35 +0300

    Added XML/XSD, cosmetic fixes

--- cms/generate_page.py
+++ cms/generate_page.py
@@ -3,41 +3,42 @@
 from markdown import markdown as md
 from os.path import exists, relpath
 from htmlmin import minify
+from html5print import HTMLBeautifier as hb
 import click
 
 builtin_elements = 'lists/post.list'
 builtin_nav_units = 'site/nav-units.html'
 
 @click.command()
-@click.option('-t', '--title', required=True, help='Embedded title used for HTML generation and file naming')
+@click.option('-t', '--title', required=True, help='Embedded title used for HTML generation and file naming', metavar='string')
 @click.option('-y', '--assume-yes', type=click.BOOL, help='Automatically overwrites files, formats your hard drive and travels back in time to kill your grandma. Without confirmation.')
-@click.option('-e', '--elements-path', type=click.Path(exists=True), required=False, help='Alternative elements file to synthesize a different page', )
+@click.option('-m/-r', '--minified-page/--readable-page', help='Toggle minification of page for development/ethical purposes', default=True)
+@click.option('-e', '--elements-path', type=click.Path(exists=True), required=False, help='Alternative elements file to synthesize a different page', metavar='elements.list')
 @click.option('-n', '--nav-units-path', type=click.Path(exists=True), required=False, help='Alternate existing nav elements file', metavar='file.html')
-@click.argument('in_file', type=click.Path(exists=True), metaver='file.md')
+@click.argument('in_file', type=click.Path(exists=True), metavar='file.md')
 @click.argument('out_file', type=click.Path(exists=True), required=False, metavar='file.html')
-def trunk(in_file, out_file, title, assume_yes, elements_path, nav_units_path):
+def trunk(in_file, out_file, title, assume_yes, elements_path, nav_units_path, minified_page):
     'Generate a sigle page from a markdown file IN_FILE using a template list and output the resulting HTML to OUT_FILE'
-    if not exists('./.magic'):
-        click.confirm('This script is supposed to be run from the website root. Are you sure you want to continue?', abort=True)
-    if not out_file:
-        out_file = 'site/posts/post_' + title + '.html'
-    if not elements_path:
-        global builtin_elements
-        elements_path = builtin_elements
-    if not nav_units_path:
-        global builtin_nav_units
-        nav_units_path = builtin_nav_units
-    if exists(out_file) and not assume_yes:
-        click.confirm('A file at ' + out_file +
-                      ' already exists, do you want to continue?', abort=True)
+    if not exists('./.magic'): click.confirm('This script is supposed to be run from the website root. Are you sure you want to continue?', abort=True)
+    if not out_file: out_file = 'site/posts/post_' + title + '.html'
+    if not elements_path: global builtin_elements;    elements_path = builtin_elements
+    if not nav_units_path: global builtin_nav_units;    nav_units_path = builtin_nav_units
+    if exists(out_file) and not assume_yes: click.confirm('A file at ' + out_file + ' already exists, do you want to continue?', abort=True)
     
     in_md = open(in_file, 'r').read()
     out_html = open(out_file, 'w')
     elements = open(elements_path, 'r').readlines()
     nav_units = ''.join(open(nav_units_path, 'r').readlines())
+    
+    print('Synthesizing page using:\n    Elements at:', elements_path, '\n    Title:\t', '"' + title + '"', '\n    Content from:', in_file, '\n    Output:\t', out_file, '\n    Page is ', end='', sep='\t')
+    if minified_page: print('minified')
+    else: print('pretty-printed')
+    print() 
 
     html_main = md(in_md)
-    result_html = minify(synthesize_page(elements, title, nav_units, nav_units_path, html_main, out_file))
+    result_html = synthesize_page(elements, title, nav_units, nav_units_path, html_main, out_file)
+    if minified_page: result_html = minify(result_html)
+    else: result_html = hb.beautify(result_html, 2)
     out_html.write(result_html)
 
 
@@ -45,7 +46,7 @@ def synthesize_page(elements, title, nav_units, nav_units_path, html_main, out_f
     result_html = ''
     for i in elements:
         wip_template = open(i.rstrip('\n'), 'r').read()
-        print(i)
+        print('Concatenating template:', i, end='')
         wip_template = wip_template.format(title=title, nav_units=synthesize_nav(nav_units, nav_units_path, title, out_file), main=html_main)
         result_html += wip_template + '\n'
     return result_html
