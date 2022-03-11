bb332fc93a4cd34481fd59683c6d2d3cc69648da
Agvantibo <derez08@yandex.ru>
2022-03-11 09:42:50 +0300

    Added docstrings, verified NT 10 compatibility

--- cms/generate_page.py
+++ cms/generate_page.py
@@ -11,11 +11,12 @@ builtin_nav_units = 'site/nav-units.html'
 @click.command()
 @click.option('-t', '--title', required=True, help='Embedded title used for HTML generation and file naming')
 @click.option('-y', '--assume-yes', type=click.BOOL, help='Automatically overwrites files, formats your hard drive and travels back in time to kill your grandma. Without confirmation.')
-@click.option('-e', '--elements-path', type=click.Path(exists=True), required=False, help='Alternative elements file to synthesize a different page')
-@click.option('-n', '--nav-units-path', type=click.Path(exists=True), required=False, help='Alternate existing nav elements file')
-@click.argument('in_file', type=click.Path(exists=True))
-@click.argument('out_file', type=click.Path(exists=True), required=False)
+@click.option('-e', '--elements-path', type=click.Path(exists=True), required=False, help='Alternative elements file to synthesize a different page', )
+@click.option('-n', '--nav-units-path', type=click.Path(exists=True), required=False, help='Alternate existing nav elements file', metavar='file.html')
+@click.argument('in_file', type=click.Path(exists=True), metaver='file.md')
+@click.argument('out_file', type=click.Path(exists=True), required=False, metavar='file.html')
 def trunk(in_file, out_file, title, assume_yes, elements_path, nav_units_path):
+    'Generate a sigle page from a markdown file IN_FILE using a template list and output the resulting HTML to OUT_FILE'
     if not exists('./.magic'):
         click.confirm('This script is supposed to be run from the website root. Are you sure you want to continue?', abort=True)
     if not out_file:
