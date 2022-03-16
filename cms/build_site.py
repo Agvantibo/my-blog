539840bfa2616caff321aa7a2661ff56ed5b2527
agvantibo <apicalium@gmail.com>
2022-03-16 21:47:35 +0300

    Added poetry, started writing batch build script

--- cms/build_site.py
+++ cms/build_site.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import click
+import os
 import yaml as ym
 
 @click.command()
-@click.argument('batch_config', type=click.Path(exists=True), required=False)
-
-def trunk(batch_config):
-    pass
+@click.option('-r', '--override-root', help='Override the site generation root')
+@click.option('-b', '--build-directory', help='Specifies the site build output directory')
+@click.argument('site_config', type=click.Path(exists=True), required=False)
+def trunk(override_root, build_directory, batch_config):
+    
